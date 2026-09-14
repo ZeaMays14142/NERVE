Fix FASTA header normalization
Prevent failures and incorrect protein matching with non-UniProt FASTA headers.

code/Adhesin.py
@@ -3,7 +3,7 @@
 import os
 import numpy as np
 from tensorflow import keras
-from code.Utils import bashCmdMethod
+from code.Utils import bashCmdMethod, build_id_index, match_protein
 
 def extract_features(list_of_proteins, NERVE_dir, iFeature_dir, working_dir, proteome1) -> list:
     """Extract features from protein sequences with iFeature and store them as numpy array in Protein.model_raw_data for subsequent preprocessing and model prediction. These features will be used by adhesin and virulent factor predictos.
@@ -21,16 +21,15 @@ def extract_features(list_of_proteins, NERVE_dir, iFeature_dir, working_dir, pro
         bashCmdMethod(f"python3 {os.path.join(iFeature_dir, 'iFeature.py')} --file {proteome1} --type {feature}\
         --out {os.path.join(working_dir, feature+'.out')}")
     # parse files and update Protein entires
+    protein_index = build_id_index(list_of_proteins)
     for i in range(len(features)):
         with open(os.path.join(working_dir, features[i]+extension)) as f:
             lines = f.readlines()[1:]
             for line in lines:
                 information = line.split('\t')
-                # Append to the correct protein
-                for protein in list_of_proteins:
-                    if information[0] in protein.id:
-                        protein.model_raw_data.append(np.array([float(el) for el in information[1:]]))
-                        break
+                protein = match_protein(information[0], protein_index, list_of_proteins)
+                if protein is not None:
+                    protein.model_raw_data.append(np.array([float(el) for el in information[1:]]))
 
     # delete files after computation
     for file in features: